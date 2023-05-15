a4c44fa7a3d56b5872afa5ed60ba408dc2f5109c
Yan Golovchenko <y4nweb@gmail.com>
2023-05-15 21:24:40 +0300

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Layout } from "antd";
+import "antd/dist/reset.css";
 
-function App() {
+const { Content } = Layout;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Layout
+        style={{
+          alignItems: "center",
+          height: "100vh",
+          position: "relative",
+        }}
+      >
+        <Content
+          style={{
+            position: "absolute",
+            top: "50%",
+            transform: "translate(0, -50%)",
+            width: 500,
+            padding: 24,
+            margin: 0,
+            height: 280,
+            background: "red",
+            borderRadius: 6,
+            border: "1px solid black",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Primary Button
+        </Content>
+      </Layout>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Search: React.FC = () => {
+  return <div>:React.FC</div>;
+};
+
+export { Search };
